7e031bcf92d806dcf7037d39f3913213f2b0fa8d
Jeremy <jeremy.bertin76@hotmail.fr>
2019-12-09 16:17:05 +0100

    you can now navigate thru 3 pages with pagination number or arrows

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const BUTTON = document.getElementById("search-button");
 const PREV = document.getElementById("prev");
 const NEXT = document.getElementById("next");
 const LINKS = document.querySelectorAll(".page");
+// console.log(LINKS);
+const CURRENT = document.querySelector(".current");
 let currentPage = 1;
 // console.log(LINKS);
 // console.log(LINKS.length);
@@ -11,32 +13,29 @@ changeNumColor = () => {
         // console.log(item);
 
         item.addEventListener('click', function () {
-          //reset the color of other links
-          LINKS.forEach(function (item) {
-            // console.log(item);
-            item.classList.remove("current");
-            item.style.backgroundColor = "#f9f9f9";
-            item.style.color = "#007BFF";
-          })
-          // apply the style to the link
-          this.classList.add("current");
-          this.style.backgroundColor = "#007BFF";
-          this.style.color = "#f9f9f9";
-          console.log(this);
-          
-          if (currentPage < this.innerHTML) {
-            nextPage();
-          } else if(currentPage == this.innerHTML) {
-            currentPage == this.innerHTML;
-            getPhoto();
-          } else if (currentPage > this.innerHTML){
-            previousPage();
-          }
+            //reset the color of other links
+            LINKS.forEach(function (item) {
+
+                item.classList.remove("current");
+            })
+            // apply the style to the link
+            this.classList.add("current");
+            // console.log(this);
+
+            if (currentPage < Number(this.innerHTML) || currentPage > Number(this.innerHTML)) {
+                currentPage = "";
+                currentPage = Number(this.innerHTML);
+                getPhoto();
+                console.log(currentPage);
+            } else if (currentPage == Number(this.innerHTML)) {
+                return currentPage;
+                getPhoto();
+            }
+            console.log(Number(this.innerHTML));
         });
-      })
+    })
 
 }
-
 changeNumColor();
 
 
@@ -47,7 +46,7 @@ generateApiUrl = () => {
     const PER_PAGE = 20;
 
     const UNSPLASHURL = `https://api.unsplash.com/search/collections/?query=${SEARCH}&client_id=${KEY}&per_page=${PER_PAGE}&page=${currentPage}`;
-    console.log(SEARCH);
+    // console.log(SEARCH);
 
     // console.log(UNSPLASHURL);
     return UNSPLASHURL;
@@ -81,23 +80,54 @@ getPhoto = () => {
         })
         .then(data => {
             loadPhotos(data);
-            // console.log(data.results[0].cover_photo.urls.small);
-            // document.getElementById("nav-pagination").style.display = "block";
+            document.getElementById("nav-pagination").style.display = "block";
         });
 }
 
+changeNumColorOnNext = () => {
+    if (LINKS[0].classList.contains("current")) {
+        LINKS[0].classList.remove("current");
+        LINKS[1].classList.add("current");
+        currentPage = LINKS[1].tabIndex;
+        console.log(currentPage);
+        console.log(LINKS[1]);
+    } else if (LINKS[1].classList.contains("current")) {
+        LINKS[1].classList.remove("current");
+        LINKS[2].classList.add("current");
+        currentPage = LINKS[2].tabIndex;
+        console.log(currentPage);
+    };
+}
+
 nextPage = () => {
-    if (currentPage == 3){
+    if (currentPage == 3) {
         return currentPage;
     } else {
-        currentPage += 1; 
+        changeNumColorOnNext();
         getPhoto();
     }
 }
 
+changeNumColorOnPrevious = () => {
+    if (LINKS[2].classList.contains("current")) {
+        LINKS[2].classList.remove("current");
+        LINKS[1].classList.add("current");
+        currentPage = LINKS[1].tabIndex;
+    } else if (LINKS[1].classList.contains("current")) {
+        LINKS[1].classList.remove("current");
+        LINKS[0].classList.add("current");
+        currentPage = LINKS[0].tabIndex;
+        // console.log(LINKS[2]);
+    };
+}
+
 previousPage = () => {
-    currentPage -= 1;
-    getPhoto();
+    if (currentPage == 1) {
+        return currentPage;
+    } else {
+        changeNumColorOnPrevious();
+        getPhoto();
+    }
 }
 
 
